20dbb390f2638b6d08155a1559fa2eeb18e22e98
Sina Mathew <mathewoluwashinastephen@gmail.com>
2024-01-16 19:47:13 +0100

    Revert commit

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,57 +11,6 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self, cls=None):
-        """Returns a list of all objects, or a dictionary if cls is provided."""
-        if cls is None:
-            return list(self.__objects.values())
-        else:
-            return {key: obj for key, obj in self.__objects.items() if isinstance(obj, cls)}
-
-
-
-    def new(self, obj):
-        """Adds new object to the storage dictionary."""
-        self.__objects[obj.to_dict()['__class__'] + '.' + obj.id] = obj
-
-    def save(self):
-        """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            for key, obj in self.__objects.items():
-                temp[key] = obj.to_dict()
-            json.dump(temp, f)
-
-    def reload(self):
-        """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
-        try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                    self.__objects[key] = classes[val['__class__']](**val)
-        except FileNotFoundError:
-            pass
-
-    def delete(self, obj=None):
-        """Deletes an object from the storage.
-
-        Args:
-            obj: The object to delete. If None, does nothing.
-=======
     # Public instance method
     def all(self):
         """ returns the dictionary __objects """
@@ -93,7 +42,6 @@ class FileStorage:
         self.save()
 
     def reload(self):
->>>>>>> 928eb9037ad9b6e471bdbcd092b58e16e47e9cb3
         """
             deserializes the JSON file to __objects (only if the JSON file
             (__file_path) exists ; otherwise, do nothing. If the file doesn’t
