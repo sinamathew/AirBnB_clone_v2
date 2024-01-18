bd4d19a18928b4cc9a1302180fdfa84544a36095
SundayMba <sundinoh@gmail.com>
2024-01-18 11:04:56 +0100

    fix review

--- models/review.py
+++ models/review.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
 
 
-class Review(BaseModel, Base):
+class Review(BaseModel):
     """ Review classto store review information """
-    __tablename__ = "reviews"
-    place_id = Column(String(60), nullable=False, foreign_key("places.id"))
-    user_id = Column(String(60), nullable=False, foreign_key("users.id"))
-    text = Column(String(1024), nullable=False)
+    #  __tablename__ = "reviews"
+    # place_id = Column(String(60), nullable=False, foreign_key("places.id"))
+    # user_id = Column(String(60), nullable=False, foreign_key("users.id"))
+    # text = Column(String(1024), nullable=False)
