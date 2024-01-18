dfe54279b207bb4493cfc8038bf68ce8e25a11b4
Sina Mathew <mathewoluwashinastephen@gmail.com>
2024-01-18 12:53:36 +0100

    Fixed Review

--- models/review.py
+++ models/review.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel
-from models.base_model import Base
+"""Review class"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    #  __tablename__ = "reviews"
-    # place_id = Column(String(60), nullable=False, foreign_key("places.id"))
-    # user_id = Column(String(60), nullable=False, foreign_key("users.id"))
-    # text = Column(String(1024), nullable=False)
+class Review(BaseModel, Base):
+    """Class for Review
+    Attributes:
+        place_id: place id
+        user_id: user id
+        text: review description
+    """
+    __tablename__ = "reviews"
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
